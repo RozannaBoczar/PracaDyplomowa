5148afe92be44ecdb7d852a9c24e055d9bc919bd
rozanna <rozanna.boczar@gmail.com>
2020-07-16 19:46:54 +0200

    You nice, keep going :D

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="Hi Rozia")
+greeting = tk.Label(text="Hi Rozia",
+                    foreground='black',
+                    background='white',
+                    width="50",
+                    height="25")
+button_detect = tk.Button(text="Detect!")
 greeting.pack()
+button_detect.pack()
 window.mainloop()
